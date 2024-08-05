0b6d82778e4382c5c9a53492a426efe1ec5cc9db
Pupocka <d.melnitsa@yandex.ru>
2024-08-05 12:45:58 +0300

    сдача проекта

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,54 +3,37 @@ export interface IProduct {
   description: string;
   image: string;
   title: string;
-  category: ProductCategory;
+  category: string;
   price: number;
 }
 
-export interface IOrder {
-  email: string;
-  phone: number;
-  address: string;
-  payment: PaymentType;
-  total: number;
+export interface IAppData {
+  catalog: IProduct[];
+  preview: string;
+  basket: string[];
+  order: IOrder;
+  total: string | number;
+  loading: boolean;
+}
+
+export interface IProductsList {
+  items: IProduct[];
+}
+
+export interface IOrderForm {
+  payment?: string;
+  address?: string;
+  phone?: string;
+  email?: string;
+  total?: string | number;
+}
+
+export interface IOrder extends IOrderForm {
   items: string[];
 }
 
-export interface IProductsData{
-  items: IProduct[];
-  preview: string | null;
-  getProduct(productId: string): IProduct;
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface IOrderResult {
+  id: string;
 }
-
-export interface IOrderData{
-  getOrderInfo(): IOrder;
-  setOrderInfo(userData: IOrder): void;
-  checkUserValidation(data: Record<keyof TUser, string>):boolean;
-}
-
-export interface IBasketData{
-  total: IProduct['price'];
-  items: IProduct['id'];
-  addProduct(product: IProduct): void;
-  deleteProduct(productId: string, playload: null): void;
-}
-
-export type TProductInfo = Pick<IProduct,  'image' | 'title' | 'category' | 'price'>
-
-export type TProductModalInfo = Pick<IProduct, 'description' | 'image' | 'title' | 'category' | 'price'>
-
-export type TOrderModalPaymentAddress = Pick<IOrder, 'payment' | 'address'>
-
-export type TOrderModalEmailPhone = Pick<IOrder, 'email' | 'phone'>
-
-export type TUser = Pick<IOrder, 'email' | 'phone' | 'address' | 'payment'>
-
-export enum ProductCategory {
-  'софт-скил' = 'soft',
-  'другое' = 'other',
-  'хард-скил' = 'hard',
-  'дополнительное' = 'additional',
-  'кнопка' = 'button'
-}
-
-export type PaymentType = 'card' | 'cash';
